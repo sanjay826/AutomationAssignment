c99cd33b8117bc62b37fcbecb36c095215c120ed
Sonardih <sonardihbackup@gmail.com>
2025-02-17 21:06:49 +0530

    Assignment 23 Updated

--- src/com/creatio/crm/language/basic/assignment/PrimeNumberCheck.java
+++ src/com/creatio/crm/language/basic/assignment/PrimeNumberCheck.java
@@ -5,7 +5,7 @@ public class PrimeNumberCheck {
         int n = 25; 
 
         boolean result = isPrime(n);
-        printExplanation(n, result);
+        printMessage(n, result);
     }
 
     public static boolean isPrime(int n) {
@@ -16,21 +16,22 @@ public class PrimeNumberCheck {
 
 		
         for (int i = 2; i * i <= n; i++) {
-			/* Found a factor, not prime Number */
+			/*  not prime Number */
         	
             if (n % i == 0) 
             	return false; 
         }
-        return true; // If no factors found, it's prime
+        return true;  //it's prime number
     }
 
-    public static void printExplanation(int n, boolean isPrime) {
+    public static void printMessage(int n, boolean isPrime) {
         if (isPrime) {
             System.out.println( n + " has exactly two divisors: 1 and " + n + ", making it a prime number.");
         } else if (n == 1) {
             System.out.println( n + " has only one divisor (1 itself), which is not sufficient for it to be considered prime.");
         } else {
             System.out.print( n + " has more than two divisors: 1");
+            
             for (int i = 2; i <= n; i++) {
                 if (n % i == 0) {
                     System.out.print(", " + i);
